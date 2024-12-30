07974e4859b68ce989800fa44456b7805adde6b4
Jens-Petter Sandvik <jens.petter.sandvik@gmail.com>
2024-12-30 09:53:24 +0100

    Correct keyboard list, add some colors to output for easy viewing

--- compileall.sh
+++ compileall.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
-KBDS="converter/usb_usb peej/lumberjack waterfowl splitkb/kyria splitkb/aurora 40percentclub/gherkin planck/rev6_drop"
+KBDS="converter/usb_usb peej/lumberjack waterfowl splitkb/kyria/rev1 splitkb/aurora/sweep/rev1 40percentclub/gherkin/pro_micro planck/rev6_drop"
 KM=jenspets
 
 for k in $KBDS; do
     echo 
-    echo Compiling $k
+    echo -e "\033[01;31m***\033[00m Compile keymap \033[01;32m$KM\033[00m for keyboard \033[01;32m$k\033[00m \033[01;31m***\033[00m" 
 
     qmk compile -kb $k -km $KM
-    
+
     ret=$?
     if [[ $ret != 0 ]]; then
-	echo "*** Compilation failed with code $ret: $k ***"
+	echo -e "\033[01;31m***\033[00m failed with code $ret: $k \033[01;31m***\033[00m"
 	break
     fi
 done;
